1682091ec1a66585c733ab079a2d53904f83a8bf
gasho <gasho@10.128.1.82>
2018-07-23 01:54:09 +0530

    v1

---
+++ paymentwallet/src/main/java/com/capgemini/paymentwallet/paymentwallet/PaymentwalletApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.paymentwallet.paymentwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentwalletApplication.class, args);
+	}
+}
