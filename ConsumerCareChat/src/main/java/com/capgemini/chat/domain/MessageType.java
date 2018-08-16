0a356e02d409bb212c374cd74c5ff8abe84497df
gassrini <gassrini@DIN66007758.corp.capgemini.com>
2018-08-16 18:24:15 +0530

    chat

---
+++ ConsumerCareChat/src/main/java/com/capgemini/chat/domain/MessageType.java
@@ -0,0 +1,7 @@
+package com.capgemini.chat.domain;
+
+public enum MessageType {
+	CHAT,
+    JOIN,
+    LEAVE
+}
---
+++ ConsumerCareChat/src/main/java/com/capgemini/chat/domain/User.java
@@ -0,0 +1,14 @@
+package com.capgemini.chat.domain;
+
+public class User {
+	private String username;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
